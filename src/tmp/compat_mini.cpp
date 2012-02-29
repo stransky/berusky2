9e2891443b0ec9935dd0db2ec4e0798a4054f6b3
Martin Stransky <stransky@anakreon.cz>
2012-02-29 23:04:56 +0100

    Update version to 0.6

---
+++ src/tmp/compat_mini.cpp
@@ -0,0 +1,133 @@
+/*
+ *        .þÛÛþ þ    þ þÛÛþ.     þ    þ þÛÛÛþ.  þÛÛÛþ .þÛÛþ. þ    þ
+ *       .þ   Û Ûþ.  Û Û   þ.    Û    Û Û    þ  Û.    Û.   Û Ûþ.  Û
+ *       Û    Û Û Û  Û Û    Û    Û   þ. Û.   Û  Û     Û    Û Û Û  Û
+ *     .þþÛÛÛÛþ Û  Û Û þÛÛÛÛþþ.  þþÛÛ.  þþÛÛþ.  þÛ    Û    Û Û  Û Û
+ *    .Û      Û Û  .þÛ Û      Û. Û   Û  Û    Û  Û.    þ.   Û Û  .þÛ
+ *    þ.      þ þ    þ þ      .þ þ   .þ þ    .þ þÛÛÛþ .þÛÛþ. þ    þ
+ *
+ * Berusky (C) AnakreoN
+ * Martin Stransky <stransky@anakreon.cz> 
+ * Michal Simonik <simonik@anakreon.cz> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "compat_mini.h"
+
+dword karmin_aktivni = 1;
+
+void ZeroMemory(void *mem, int size)
+{
+  memset(mem, 0, size);
+}
+
+char *itoa(int d, char *buf, int base)
+{
+  char *p = buf;
+  char *p1, *p2;
+  unsigned long ud = d;
+  int divisor = 10;
+
+/* If %d is specified and D is minus, put `-' in the head. */
+  if (base == 'd' && d < 0) {
+    *p++ = '-';
+    buf++;
+    ud = -d;
+  }
+  else if (base == 'x')
+    divisor = 16;
+
+  /* Divide UD by DIVISOR until UD == 0. */
+  do {
+    int remainder = ud % divisor;
+
+    *p++ = (remainder < 10) ? remainder + '0' : remainder + 'A' - 10;
+  }
+  while (ud /= divisor);
+
+  /* Terminate BUF. */
+  *p = 0;
+
+  /* Reverse BUF. */
+  p1 = buf;
+  p2 = p - 1;
+  while (p1 < p2) {
+    char tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+    p1++;
+    p2--;
+  }
+
+  return(buf);
+}
+
+THREAD_HANDLE CreateThread(void *lpThreadAttributes, size_t dwStackSize, thread_routine lpStartAddress,
+                           void * lpParameter, dword dwCreationFlags, THREAD_ID *lpThreadId)
+{  
+  THREAD_HANDLE handle;
+  lpThreadId = NULL;
+  pthread_create(&handle, NULL, lpStartAddress, lpParameter);
+  return(handle);
+}
+
+int CloseHandle(THREAD_HANDLE handle)
+{
+
+}
+
+void ExitThread(dword dwExitCode)
+{
+  int ret;
+  pthread_exit(&ret);
+}
+
+int SetThreadPriority(THREAD_HANDLE hThread, int nPriority)
+{
+}
+
+int GetThreadPriority(THREAD_HANDLE hThread)
+{
+}
+
+int GetExitCodeThread(THREAD_HANDLE hThread, dword *lpExitCode)
+{
+  *lpExitCode = 0;
+  return(pthread_join(hThread, NULL));
+}
+
+void newline_cut(char *p_line)
+{
+  int len = strlen(p_line);
+  if(len > 1 && (p_line[len-1] == '\n' || p_line[len-1] == '\r')) {
+    p_line[len-1] = '\0';
+    len--;
+  
+    if(len > 1 && (p_line[len-1] == '\n' || p_line[len-1] == '\r')) {
+      p_line[len-1] = '\0';
+      len--;
+    }
+  }
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/tmp/compat_mini.h
@@ -0,0 +1,193 @@
+/*
+ *        .þÛÛþ þ    þ þÛÛþ.     þ    þ þÛÛÛþ.  þÛÛÛþ .þÛÛþ. þ    þ
+ *       .þ   Û Ûþ.  Û Û   þ.    Û    Û Û    þ  Û.    Û.   Û Ûþ.  Û
+ *       Û    Û Û Û  Û Û    Û    Û   þ. Û.   Û  Û     Û    Û Û Û  Û
+ *     .þþÛÛÛÛþ Û  Û Û þÛÛÛÛþþ.  þþÛÛ.  þþÛÛþ.  þÛ    Û    Û Û  Û Û
+ *    .Û      Û Û  .þÛ Û      Û. Û   Û  Û    Û  Û.    þ.   Û Û  .þÛ
+ *    þ.      þ þ    þ þ      .þ þ   .þ þ    .þ þÛÛÛþ .þÛÛþ. þ    þ
+ *
+ * Berusky (C) AnakreoN
+ * Martin Stransky <stransky@anakreon.cz> 
+ * Michal Simonik <simonik@anakreon.cz> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef __COMPAT_MINI_H__
+#define __COMPAT_MINI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "compatibility.h"
+
+#include <string.h>
+#include <limits.h>
+#include <wchar.h>
+#ifndef WINDOWS
+#include <pthread.h>
+#endif
+
+#define  MAX_JMENO       50
+
+#define  TRUE   (1==1)
+#define  FALSE  (1!=1)
+
+#ifdef WINDOWS
+#define DIR_SLASH         '\\'
+#define DIR_SLASH_STRING  "\\"
+#elif LINUX
+#define DIR_SLASH         '/'
+#define DIR_SLASH_STRING  "/"
+#endif
+
+typedef unsigned char byte;
+typedef unsigned char BYTE;
+typedef unsigned short word;
+typedef unsigned short WORD;
+typedef unsigned int dword;
+typedef unsigned int COLORREF;
+typedef long LONG;
+typedef dword DWORD;
+typedef float FLOAT;
+typedef wchar_t WCHAR;
+typedef void *HWND;
+typedef void *HDC;
+typedef unsigned int UINT;
+typedef int BOOL;
+typedef void *HINSTANCE;
+typedef word FACE;
+
+extern dword karmin_aktivni;
+
+// default filename size
+#define MAX_PATH 255
+
+typedef struct _MOUSE_INFO
+{
+  int x_res, y_res;
+
+  int x, y;
+  int rx, ry;
+  int dx, dy;
+
+  int t1, t2;
+  int dt1, dt2;
+  int d1, d2;
+  int tf1, tf2;
+  int lt1, lt2;
+
+} MOUSE_INFO;
+
+#define CP_ACP     0
+#define HDC2DD     -1
+#define MAKEWORD(a, b)      ((unsigned short)(((unsigned char)(a)) | ((unsigned short)((unsigned char)(b))) << 8))
+
+void kprintf(char log, const char *p_text, ...);
+void kprintfl(char log, const char *p_text, ...);
+void ddw(char *p_text, ...);
+
+#define  MAX_FILENAME_LENGTH  200
+
+#if PATH_MAX > MAX_FILENAME_LENGTH
+#define  MAX_FILENAME         MAX_FILENAME_LENGTH
+#else
+#define  MAX_FILENAME         PATH_MAX
+#endif
+
+extern char ini_file[MAX_FILENAME];
+
+void Sleep(int ms);
+
+char *itoa(int d, char *buf, int base);
+
+void timeGetTimeInit(void);
+unsigned int timeGetTime(void);
+
+int GetFileSize(FILE *f, dword *size);
+void ZeroMemory(void *mem, int size);
+
+void newline_cut(char *p_line);
+
+#ifndef WINDOWS
+enum
+{
+  EAX_ENVIRONMENT_GENERIC,
+  EAX_ENVIRONMENT_PADDEDCELL,
+  EAX_ENVIRONMENT_ROOM,
+  EAX_ENVIRONMENT_BATHROOM,
+  EAX_ENVIRONMENT_LIVINGROOM,
+  EAX_ENVIRONMENT_STONEROOM,
+  EAX_ENVIRONMENT_AUDITORIUM,
+  EAX_ENVIRONMENT_CONCERTHALL,
+  EAX_ENVIRONMENT_CAVE,
+  EAX_ENVIRONMENT_ARENA,
+  EAX_ENVIRONMENT_HANGAR,
+  EAX_ENVIRONMENT_CARPETEDHALLWAY,
+  EAX_ENVIRONMENT_HALLWAY,
+  EAX_ENVIRONMENT_STONECORRIDOR,
+  EAX_ENVIRONMENT_ALLEY,
+  EAX_ENVIRONMENT_FOREST,
+  EAX_ENVIRONMENT_CITY,
+  EAX_ENVIRONMENT_MOUNTAINS,
+  EAX_ENVIRONMENT_QUARRY,
+  EAX_ENVIRONMENT_PLAIN,
+  EAX_ENVIRONMENT_PARKINGLOT,
+  EAX_ENVIRONMENT_SEWERPIPE,
+  EAX_ENVIRONMENT_UNDERWATER,
+  EAX_ENVIRONMENT_DRUGGED,
+  EAX_ENVIRONMENT_DIZZY,
+  EAX_ENVIRONMENT_PSYCHOTIC,
+
+  EAX_ENVIRONMENT_COUNT
+};
+
+#define THREAD_PRIORITY_TIME_CRITICAL 1
+#define THREAD_PRIORITY_HIGHEST       2
+#define THREAD_PRIORITY_ABOVE_NORMAL  3
+#define THREAD_PRIORITY_NORMAL        4
+#define THREAD_PRIORITY_BELOW_NORMAL  5
+#define THREAD_PRIORITY_LOWEST        6
+#define THREAD_PRIORITY_IDLE          7
+
+#define STILL_ACTIVE                  259
+
+typedef unsigned long THREAD_ID;
+typedef pthread_t THREAD_HANDLE;
+
+typedef void * (*thread_routine)(void *);
+ 
+THREAD_HANDLE CreateThread(void *lpThreadAttributes, size_t dwStackSize, thread_routine lpStartAddress,
+                           void * lpParameter, dword dwCreationFlags, THREAD_ID *lpThreadId);
+int  CloseHandle(THREAD_HANDLE handle);
+void ExitThread(dword dwExitCode);
+int  SetThreadPriority(THREAD_HANDLE hThread, int nPriority);
+int  GetThreadPriority(THREAD_HANDLE hThread);
+int  GetExitCodeThread(THREAD_HANDLE hThread, dword *lpExitCode);
+
+#else
+
+typedef unsigned long THREAD_ID;
+typedef void * THREAD_HANDLE;
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __COMPAT_MINI_H__
