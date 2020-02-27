stransky/berusky2

commit ed7356f5976f73e1cb9066e4e7752c988c46f885

Don't trigger an assertion failure on zero-length camera movements.
A zero-length camera movement can occur when a bug is selected which
is already selected (for example, clicking on the selected bug in the
navigation bar or pressing TAB on a level with only one bug) and "move
cam with bug" is enabled.

Also, reset 'p_ber->kamnum' to zero even if '!(p_ber->kamera.aktivni &
GAME_KAMERA_ANIMACE)'. This is because for a zero-length camera
movement, 'p_ber->kamera.aktivni & GAME_KAMERA_ANIMACE' will be zero
when kam_stop() is called.

src/komat/Keyframe.h
@@ -249,7 +249,7 @@ inline int calc_time_akt(int next_time, int time_start)
 
 inline int calc_endtime(int framenum)
 {
-  return ((framenum - 1) * SIM_KONSTI);
+  return (framenum * SIM_KONSTI);
 }
 
 inline int calc_keynum(int endtime)

src/komat/Berusky3d_kamery.cpp
@@ -902,12 +902,12 @@ RunHandle kam_start(AnimHandle a_handle, int *p_flag, int flag, int start, int s
 
 void kam_stop(void)
 {
-  if (p_ber->kamera.aktivni & GAME_KAMERA_ANIMACE) {
-    if (p_ber->kamera.flag & GK_REMOVE) {
-      kam_zrus_animaci(p_ber->kamera.a_handle);
-      p_ber->kamnum = 0;
-    }
+  if (p_ber->kamera.flag & GK_REMOVE) {
+    kam_zrus_animaci(p_ber->kamera.a_handle);
+    p_ber->kamnum = 0;
+  }
 
+  if (p_ber->kamera.aktivni & GAME_KAMERA_ANIMACE) {
     p_ber->kamera.aktivni &= ~GAME_KAMERA_ANIMACE;
     p_ber->kamera.p_anim = NULL;
     p_ber->kamera.a_handle = K_CHYBA;

commit e48218ee4149933454213bae21414163c2f2ed77

Fix slight pause between walking animations.
This was caused in commit ed7356f when I removed the -1 from
calc_endtime(). I did this because it was the simplest way to ensure
it would not return a negative number, and I didn't notice any
negative effects at the time. However, it turns out that this caused a
slight pause between bug walking animations (when a bug steps to a new
square). So re-add the -1 (but still make sure we don't return a
negative number). Also add a comment to avoid confusion in the future.

src/komat/Keyframe.h
@@ -249,7 +249,10 @@ inline int calc_time_akt(int next_time, int time_start)
 
 inline int calc_endtime(int framenum)
 {
-  return (framenum * SIM_KONSTI);
+  // Leave the -1 or there will be a slight pause between walking
+  // animations. If `framenum' is 0, we have to return 0 because
+  // otherwise we would return a negative number.
+  return framenum ? ((framenum - 1) * SIM_KONSTI) : 0;
 }
 
 inline int calc_keynum(int endtime)