stransky/berusky2

commit 9ce74f56d81f2bc077bdcafc7b724e61e5aacd99

Change when camera movement is allowed.
Allow camera movement when a bug is moving and don't allow camera
movement during the camera intro when playing a demo.

src/kofola/game_logic.cpp
@@ -11994,8 +11994,6 @@ PLAY_LEVEL_START:
 
       mi.t1 = 0;
     }
-    else                        // nemuze se pouzivat pr ovladani kamery
-      PRCameraFlag = 0;
 
     if (mi.t1 && (Level.bRestart || Level.bReturnToMenu)) {
       if (Level.bRestart && Level.iMessageBoxReturn != -1) {
@@ -12040,9 +12038,6 @@ PLAY_LEVEL_START:
 
       mi.t1 = 0;
     }
-
-    if (demo)
-      PRCameraFlag = 1;
 
     if (iMenuAnimation) {
       iMenuAnimation = 0;

commit 4e966f0ea2676cb4163c8f65666823e62c60e29c

Don't allow camera movement right after the camera intro when the camera is still moving.
This was caused by commit 9ce74f5.

src/kofola/game_logic.cpp
@@ -11018,8 +11018,6 @@ PLAY_LEVEL_START:
       if (iActualLevel >= 200 && iActualLevel < 300 && !demo)
         RunMenuTutorial2("Mmtutor3D.txt", p_ad, &Level, &am);
 
-      PRCameraFlag = 1;
-
       bCamera = 0;
     }
 